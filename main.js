2a7ed1bb9e1b43930655083e7e690cefb5b36fed
Gunalp Uysal <uysalp@gmail.com>
2019-08-30 20:31:34 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// this creates the same closure as in 1-closureExample.js, but doesn't pollute
+// the global scope with a function called makeHelloFunction like that example
 const sayHello = (function () {
   var message = 'Hello!'
 
@@ -7,3 +9,18 @@ const sayHello = (function () {
 
   return sayHello
 })()
+
+// IIFEs can also be used to create variables that are inaccessible from the global
+// scope
+const counter = (function() {
+  let count = 0
+
+  return {
+    inc: function() { count = count + 1 },
+    get: function() { console.log(count) },
+  }
+})()
+
+counter.get()
+counter.inc()
+counter.get()
